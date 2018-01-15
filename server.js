c66da55ed03fba39fd4f80ff9590f1da45c0efba
Roman Frołow <rofrol@gmail.com>
2018-01-15 14:21:33 +0100

    Allow all origins

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(function (req, res, next) {
 
       // Website you wish to allow to connect
-      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+      res.setHeader('Access-Control-Allow-Origin', '*');
 
       // Request methods you wish to allow
       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
